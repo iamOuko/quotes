852e8512cb7b7a3bc96b932df583682a215312b3
iamOuko <oukoezzy@gmail.com>
2020-08-22 22:42:52 +0300

    add components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
+import { FormComponent } from './components/form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
